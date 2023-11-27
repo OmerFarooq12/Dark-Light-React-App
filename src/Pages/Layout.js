e382e09d3013f528e660460c37df43093612ad31
OmerFarooq12 <136107721+OmerFarooq12@users.noreply.github.com>
2023-11-26 19:53:54 -0800

    Add files via upload

---
+++ src/Pages/Layout.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Footer from '../Components/Footer'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+        <Outlet />
+        <Footer />
+    </>
+  )
+}
